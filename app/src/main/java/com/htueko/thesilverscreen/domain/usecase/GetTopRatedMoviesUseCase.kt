f193a8e94e5f0f9894957dfbf44e1bc93daab752
Htue Ko <htooko1101@gmail.com>
2021-11-24 00:35:35 +0630

    add movie usecases

---
+++ app/src/main/java/com/htueko/thesilverscreen/domain/usecase/GetTopRatedMoviesUseCase.kt
@@ -0,0 +1,14 @@
+package com.htueko.thesilverscreen.domain.usecase
+
+import com.htueko.thesilverscreen.domain.model.Movie
+import com.htueko.thesilverscreen.domain.model.status.ResultOf
+import com.htueko.thesilverscreen.domain.repository.MovieRepository
+import javax.inject.Inject
+
+class GetTopRatedMoviesUseCase @Inject constructor(
+    private val repo: MovieRepository
+) {
+    suspend operator fun invoke(page: Int): ResultOf<List<Movie>> {
+        return repo.getTopRatedMovies(page)
+    }
+}
