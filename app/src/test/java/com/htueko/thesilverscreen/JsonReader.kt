9967e9f0f9750d75dd364e85fc55f7d7ca0404e2
Htue Ko <htooko1101@gmail.com>
2021-11-24 00:46:59 +0630

    add unit test for remote service

---
+++ app/src/test/java/com/htueko/thesilverscreen/JsonReader.kt
@@ -0,0 +1,19 @@
+package com.htueko.thesilverscreen
+
+import androidx.test.platform.app.InstrumentationRegistry
+import timber.log.Timber
+import java.io.IOException
+import java.io.InputStream
+
+object JsonReader {
+    fun getJson(path: String): String {
+        return try {
+            val context = InstrumentationRegistry.getInstrumentation().context
+            val jsonStream: InputStream = context.assets.open("networkresponses/$path")
+            String(jsonStream.readBytes())
+        } catch (exception: IOException) {
+            Timber.e("getJson: Error reading network response json asset")
+            throw exception
+        }
+    }
+}
---
+++ app/src/test/java/com/htueko/thesilverscreen/data/remote/HttpStatusCode.kt
@@ -0,0 +1,8 @@
+package com.htueko.thesilverscreen.data.remote
+
+import io.ktor.http.HttpStatusCode
+
+object HttpStatusCodeCollection {
+
+    val SUCCESS_200 = HttpStatusCode.OK
+}
